20fecb5d4113c4b1d843b1ecd06f97489009df24
Richard Widén <richardwiden@gmail.com>
2016-09-20 13:43:20 +0200

    inital commit

--- app/routes/things.js
+++ app/routes/things.js
@@ -3,6 +3,7 @@
 //TODO read from db
 module.exports = function (server) {
     var Thing= require('../models/thing');
+
     server.get('/things', listThings);
     server.post('things', listThings);
     function listThings(req, res, next) {
